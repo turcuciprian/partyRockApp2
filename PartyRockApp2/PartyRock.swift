f18282b32880af5ba9bfd606a453255c8992a7d7
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-10 01:13:48 +0300

    created a new swift file with a new class for the values of the cell

---
+++ PartyRockApp2/PartyRock.swift
@@ -0,0 +1,22 @@
+//
+//  PartyRock.swift
+//  PartyRockApp2
+//
+//  Created by Turcu Ciprian on 10/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import Foundation
+
+class PartyRock{
+    private var _imageURL: String
+    private var _videoURL: String
+    private var _videoTitle: String
+    
+    init(imageURL: String, videoURL: String, videoTitle: String){
+        _imageURL = imageURL
+        _videoURL = videoURL
+        _videoTitle = videoTitle
+        
+    }
+}
