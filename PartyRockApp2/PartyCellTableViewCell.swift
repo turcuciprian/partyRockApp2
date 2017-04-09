19aa6a9d6fff1a37a60cc2517015c35f8cdc5216
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-10 00:42:57 +0300

    Added partyCell removed set selected func

---
+++ PartyRockApp2/PartyCellTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PartyCellTableViewCell.swift
+//  PartyRockApp2
+//
+//  Created by Turcu Ciprian on 10/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import UIKit
+
+class PartyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
