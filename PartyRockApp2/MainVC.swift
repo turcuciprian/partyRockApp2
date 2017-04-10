4fd42593c981a9e7fe1ea3984f7bcd7d166cdf15
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-10 23:35:55 +0300

    added 5 video data sources, p1, p2, p3, p4, p5 and a function to generate the embed code string based on the youtube video code

--- PartyRockApp2/MainVC.swift
+++ PartyRockApp2/MainVC.swift
@@ -16,11 +16,37 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let imgURL = "https://i.ytimg.com/vi/2SH0pVVBmN0/maxresdefault.jpg"
-        let urlTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/2bF6QipA3iQ\" frameborder=\"0\" allowfullscreen></iframe>"
+        var imgURL = "https://i.ytimg.com/vi/2SH0pVVBmN0/maxresdefault.jpg"
+        var urlTest = self.return_embeded(ytCode: "2bF6QipA3iQ")
         let p1 = PartyRock(imageURL: imgURL, videoURL: urlTest, videoTitle: "love is a one way street, live")
         
-//        PartyRocks.append(p1)
+        PartyRocks.append(p1)
+        
+        imgURL = "https://i.ytimg.com/vi/2SH0pVVBmN0/maxresdefault.jpg"
+        urlTest = self.return_embeded(ytCode: "2bF6QipA3iQ")
+        let p2 = PartyRock(imageURL: imgURL, videoURL: urlTest, videoTitle: "love is a one way street, live")
+        
+        PartyRocks.append(p2)
+        
+        imgURL = "https://i.ytimg.com/vi/2SH0pVVBmN0/maxresdefault.jpg"
+        urlTest = self.return_embeded(ytCode: "2bF6QipA3iQ")
+        let p3 = PartyRock(imageURL: imgURL, videoURL: urlTest, videoTitle: "love is a one way street, live")
+        
+        PartyRocks.append(p3)
+        
+        imgURL = "https://i.ytimg.com/vi/2SH0pVVBmN0/maxresdefault.jpg"
+        urlTest = self.return_embeded(ytCode: "2bF6QipA3iQ")
+        let p4 = PartyRock(imageURL: imgURL, videoURL: urlTest, videoTitle: "love is a one way street, live")
+        
+        PartyRocks.append(p4)
+        
+        imgURL = "https://i.ytimg.com/vi/2SH0pVVBmN0/maxresdefault.jpg"
+        urlTest = self.return_embeded(ytCode: "2bF6QipA3iQ")
+        let p5 = PartyRock(imageURL: imgURL, videoURL: urlTest, videoTitle: "love is a one way street, live")
+        
+        PartyRocks.append(p5)
+        
+        
         
         
         
@@ -38,6 +64,10 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             return UITableViewCell()
         }
         return UITableViewCell()
+    }
+    func return_embeded(ytCode: String) -> String{
+        return "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/\(ytCode)\" frameborder=\"0\" allowfullscreen></iframe>"
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return PartyRocks.count
